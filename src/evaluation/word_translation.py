501d4e7967f885e7ee33737b92d94fa8d5730461
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-01-30 15:22:04 +0100

    word_translation.py comments

--- src/evaluation/word_translation.py
+++ src/evaluation/word_translation.py
@@ -124,6 +124,9 @@ def get_word_translation_accuracy(lang1, word2id1, emb1, lang2, word2id2, emb2, 
         average_dist2 = torch.from_numpy(average_dist2).type_as(emb2)
         # queries / scores
         query = emb1[dico[:, 0]]
+        # emb1 (FloatTensor) has size 200000 x 300
+        # dico[:, 0] (LongTensor) has size 2745
+        # query (FloatTensor) has size 2745 x 300
         scores = query.mm(emb2.transpose(0, 1))
         scores.mul_(2)
         scores.sub_(average_dist1[dico[:, 0]][:, None] + average_dist2[None, :])
@@ -136,6 +139,10 @@ def get_word_translation_accuracy(lang1, word2id1, emb1, lang2, word2id2, emb2, 
     for k in [1, 5, 10]:
         top_k_matches = top_matches[:, :k]
         _matching = (top_k_matches == dico[:, 1][:, None].expand_as(top_k_matches)).sum(1)
+        # dico[:, 1][:, None] (LongTensor) has size 2745 x 1
+        # top_k_matches (LongTensor) has size 2745 x k
+        # _matching (ByteTensor) has size 2745
+
         # allow for multiple possible translations
         matching = {}
         for i, src_id in enumerate(dico[:, 0]):
